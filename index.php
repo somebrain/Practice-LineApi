057c1947690429335066783ff5d670b332b93beb
abokado <somebrain.suzuki1@gmail.com>
2017-10-26 16:10:26 +0900

    テンプレートメッセージのコンファームメッセージまでできた

--- index.php
+++ index.php
@@ -13,15 +13,32 @@ foreach ($events as $event) {
     // $bot->replyText($event->getReplyToken(), 'text');
    //replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
   // replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東  京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025,139.703473);
-replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
+//replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
+//replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] .
+ //'/imgs/preview.jpg');
+
+
+$columnArray = array();
+for($i = 0; $i < 5; $i++) {
+$actionArray = array();
+array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder ('ボタン' . $i . '_' . 1, 'c-' . $i . '-' . 1));
+array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder ('ボタン' . $i . '-' . 2, 'c-' . $i . '-' . 2));
+array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder ('ボタン' . $i . '-' . 3, 'c-' . $i . '-' . 3));
+$column = new  LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
+($i + 1) . '日後の天気',
+'晴れ',
+ 
+
+
+
+
 
 
 }
-
 function replyTextMessage($bot, $replyToken, $text) {
 $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
 
-if (!$response->isSucceeded()) {
+(!$response->isSucceeded()) {
 error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
    // error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody(), 3, './error.log');
    }
@@ -46,11 +63,64 @@ function replyStickerMessage($bot, $replyToken, $packageId, $stickerId) {
 }
 
 
+function replyImageMessage($bot, $replyToken, $originalImageUrl,$previewImageUrl) {
+    $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+    if (!$response->isSucceeded()) {
+        error_log('Failed!' . $response->getHTTPStatus . ' ' .$response->getRawBody());
+    }
+}
+
+
+function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, ...$actions) {
+    $actionArray = array();
+    foreach($actions as $value) {
+        array_push($actionArray, $value);
+    }
+    $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($alternativeText,new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder($title, $text, $imageUrl, $actionArray));
+    $response = $bot->replyMessage($replyToken, $builder);
+    if (!$response->isSucceeded()) {
+        erro_log('Failded' . $response->getHTTPStatus . ' ' . $response->getRAwBody());
+
+    }
+}
+
+
+function replyConfirmTemplate($bot, $replyToken, $alternativeText, $text, ...$actions) {
+    $actionArray = array();
+    foreach($actions as $value) {
+        array_push($actionArray, $value);
+    }
+    $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($alternativeText,new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder ($text, $actionArray)
+    );
+    $response = $bot->replyMessage($replyToken, $builder);
+    if (!$response->isSucceeded()) {
+        erro_log('Failded' . $response->getHTTPStatus . ' ' . $response->getRAwBody());
+
+    }
+
+}
+
+function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray) {
+ $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($alternativeText,new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder ($columnArray)
+);
+$response = $bot->replyMessage($replyToken, $builder);
+if (!$response->isSucceeded()) {
+erro_log('Failded' . $response->getHTTPStatus . ' ' . $response->getRAwBody());
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
-
-
-
-
-　　　　　　　　　　　　　　　　　　　　　　　　　
